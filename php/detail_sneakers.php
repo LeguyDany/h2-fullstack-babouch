4d40087b44b5ee7d63b102047017848e06e6b727
BlatteSquash <noahdn@icloud.com>
2022-03-30 22:33:58 +0200

    v0.3

--- php/detail_sneakers.php
+++ php/detail_sneakers.php
@@ -1,23 +1,36 @@
-from style.css import *
-
 <!DOCTYPE html>
 
-<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    <link rel="stylesheet" href="../css/styles.css" type="text/css" >
+    <link rel="stylesheet" href="https://meyerweb.com/eric/tools/css/reset/reset.css" >
+    <link rel="stylesheet" href="../assets/font/MonumentExtended-FreeForPersonalUse/MonumentExtended-Regular.otf" type="text/css">
 </head>
 
-
 <body>
-<header>
-</header>
 <main>
-    <section ID="photo_sneakers">
-        
+<!--    Le fond de la page-->
+    <section>
+
+<!--        Le côté gauche de la page (les images)-->
+        <div id="photo_sneakers1">
+            <img id="photo_1" src="../assets/image/The%20Greatest%20%2319%20–%20%20The%20Future.png" alt="image1">
+            <div id="photo_sneakers2">
+                <img id="photo_2" src="../assets/image/The%20Greatest%20%2319%20–%20%20The%20Future.png" alt="image2">
+                <img id="photo_3" src="../assets/image/nike-dunk-low-court-purple-dd1391-104-pic04%206.png" alt="image3">
+                <img id="photo_4" src="../assets/image/nike-dunk-low-court-purple-dd1391-104-pic04%207.png" alt="image4">
+            </div>
+        </div>
+
+<!--        Le côté droit de la page (le texte)-->
+        <div>
+            <h1>Nike</h1>
+            <h2>Dunk Low Team Red</h2>
+            <hr>
+            <h4>270.00€</h4>
+        </div>
+
     </section>
 </main>
-<footer>
-</footer>
 </body>
-</html>
