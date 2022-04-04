c6d91d3669d46b9c808eeadd1bcc2d8ba0482dba
Leguy Dany <dany.leguy@hetic.net>
2022-04-04 23:00:17 +0200

    v1.7.5

---
+++ php/detail_sneakers.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="../css/styles.css" type="text/css" >
+    <link rel="stylesheet" href="https://meyerweb.com/eric/tools/css/reset/reset.css" >
+    <link rel="stylesheet" href="../assets/font/MonumentExtended-FreeForPersonalUse/MonumentExtended-Regular.otf" type="text/css">
+</head>
+
+<body>
+<main>
+<!--    Le fond de la page-->
+    <section id="un">
+
+<!--        Le côté gauche de la page (les images)-->
+        <div id="photo_sneakers1">
+            <img id="photo_1" src="../assets/image/The%20Greatest%20%2319%20–%20%20The%20Future.png" alt="image1">
+            <div id="photo_sneakers2">
+                <img id="photo_2" src="../assets/image/The%20Greatest%20%2319%20–%20%20The%20Future.png" alt="image2">
+                <img id="photo_3" src="../assets/image/nike-dunk-low-court-purple-dd1391-104-pic04%206.png" alt="image3">
+                <img id="photo_4" src="../assets/image/nike-dunk-low-court-purple-dd1391-104-pic04%207.png" alt="image4">
+            </div>
+        </div>
+
+<!--        Le côté droit de la page (le texte)-->
+        <div>
+            <h1 id="Nike">Nike</h1>
+            <h2 id="Dunk">Dunk Low Team Red</h2>
+            <hr id="Dunk">
+            <h4 id="euro">270.00€</h4>
+            <h4 id="taille">TAILLE</h4>
+            <div id="taille">
+                <h3>37.5</h3>
+                <h3>38</h3>
+                <h3>39</h3>
+                <h3 id="barre">40.5</h3>
+                <h3>42</h3>
+            </div>
+            <div id="Ajouter">
+                <h3>Ajouter au panier</h3>
+            </div>
+            <div id="Plus">
+                <p id="description">Description</p>
+                <p>Authenticité</p>
+                <p>Livraison</p>
+            </div>
+            <hr id="Plus">
+            <div id="texte">
+                <p>Directement inspirée de la version Dunk High Team Red, la nouvelle Dunk Low s'inspire de la tendance bordeaux pour le plus grand plaisir des fans de sneakers.
+                    <br><br>La Nike Dunk Low Team Red se pare d'une tige en cuir blanc, couplée à des empiècements en cuir bordeaux au niveau des œillets, du mudguard et du Swoosh. On note un branding Nike sur le talon et sur la languette. Une midsole blanche, accompagnée d'une outsole bordeaux vient parfaire l'ensemble.
+                    <br><br>Avec une palette de couleur harmonieuse, cette nouvelle Nike Dunk Low ne manquera pas de trouver son public !</p>
+            </div>
+        </div>
+    </section>
+    <section id="deux">
+        <div id="commentaire">
+            <div id="commande">
+                <h2>Commande parfaite</h2>
+                <p>Livraison dans les délais avec le <br>produit neuf (et sans défault)</p>
+            </div>
+            <div id="parfait">
+                <h2>Parfait !</h2>
+                <p>Commande parfaite, livré rapidement. <br> Produit conforme à la description et à nos attentes. <br> Je recommande le site !</p>
+            </div>
+            <div id="top">
+                <h2>Commande au top</h2>
+                <p>Parfait le délai de livraison est court <br> et les chaussures arrive nickel. Rien à dire</p>
+            </div>
+        </div>
+    </section>
+    <section id="trois">
+        <div>
+            <h1 id="Recommandations">Recommandations . 4</h1>
+            <div id="Recommandations">
+                <div id="image_1">
+                    <img id="image_red" src="../assets/image/nike-dunk-low-team-red-wethenew1_2000x%201rouge.png" alt="image_1">
+                    <h6 id="Team_red">Nike Dunk Team Red</h6>
+                    <h6 id="Team_red_2">à partir de 270€</h6>
+                </div>
+                <div id="image_2">
+                    <img id="image_yeezy" src="../assets/image/air-jordan-1-mid-paris-wethenew-1_1_2000x%201yeezy.png" alt="image_2">
+                    <h6 id="yeezy">Yeezy 700 Wave Runner <br>Solid Grey</h6>
+                    <h6 id="yeezy_2">à partir de 400€</h6>
+                </div>
+                <div id="image_3">
+                    <img id="image_purple" src="../assets/image/nike-dunk-low-court-purple-2021-DD1391-104-wethenew_2000x%201purple.png" alt="image_3">
+                    <h6 id="purple">Nike Dunk Low Court Purple</h6>
+                    <h6 id="purple_2">à partir de 220€</h6>
+                </div>
+                <div id="image_4">
+                    <img id="image_Two" src="../assets/image/nike-dunk-low-disrupt-2-malachite-DH4402-001-2-wethenew-1_2000x%201venice.png" alt="image_4">
+                    <h6 id="Two_tone">Nike Dunk Low Two Tone Grey</h6>
+                    <h6 id="Two_tone_2">à partir de 150€</h6>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+</body>
